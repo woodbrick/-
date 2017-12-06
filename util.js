bcdeb6089f579aae7a4c6f1f9ff2e4d1536cee9e
William <frankyf@126.com>
2017-12-06 21:36:31 +0800

    Update util.js

--- util.js
+++ util.js
@@ -21,3 +21,15 @@ fields[1].isValidate = () => true
 validate(fields) // true
 */
 
+const toInt = obj => ~~obj
+
+
+cons runCode = callback => {
+  try {
+    callback()
+  } catch (e) {
+    window.location.href =
+      "http://stackoverflow.com/search?q=[js]+" + e.message;
+  }
+
+}
